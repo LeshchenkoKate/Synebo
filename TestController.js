0ffe3e58d1ad555e382d159d467e52352dc3d2c8
LeshchenkoKate <34144754+LeshchenkoKate@users.noreply.github.com>
2020-02-04 17:37:58 +0200

    Update TestController.js

--- TestController.js
+++ TestController.js
@@ -1,16 +1,30 @@
 ({
-    doInit: function(component, event, helper) {
-        // Create the action
-        var str = component.get("c.accname");
-        var action = component.get("c.getExpenses");
+    doInit: function (cmp, event, helper) {
+        cmp.set('v.columns', [
+            {label: 'Id', fieldName: 'Id', type: 'text'},
+            {label: 'Account name', fieldName: 'UrlLink__c', type: 'url', typeAttributes: { label: { fieldName: 'Name'}}},
+            {label: 'Account number', fieldName: 'AccountNumber', type: 'number'},
+            {label: 'Number Of Employees', fieldName: 'NumberOfEmployees', type: 'number'},
+            {label: 'Fax', fieldName: 'Fax', type: 'phone'},
+            {label: 'Industry', fieldName: 'Industry', type: 'text'}
+           
+        ]);
+
+        var str = cmp.get("v.accname");
+        var action = cmp.get("c.getExpenses");
         action.setParams({
-            "name":str 
+            "Name1":str 
         });
         // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set("v.expenses", response.getReturnValue());
+                
+                var rec = response.getReturnValue();
+                rec.forEach(function(record){
+                    record.UrlLink__c = '/' + record.Id;
+                });
+                cmp.set("v.data", rec);
             }
             else {
                 console.log("Failed with state: " + state);
@@ -18,5 +32,9 @@
         });
         // Send action off to be executed
         $A.enqueueAction(action);
+    
     }
 })
+       
+
+        
